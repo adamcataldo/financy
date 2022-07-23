9de155db84f85f5beb18189f9168a85badd2a87f
Adam Cataldo <adam.cataldo@gmail.com>
2022-07-23 12:48:45 -0700

    fixes adamcataldo/financy#15

---
+++ config/__init__.py
@@ -0,0 +1,15 @@
+import os
+import warnings
+
+if 'LOCAL_FINANCY' not in os.environ:
+    warnings.warn('No environment variable found named $LOCAL_FINANCY. Using $HOME instead')
+    local_financy = os.environ['HOME']
+else:
+    local_financy = os.environ['LOCAL_FINANCY']
+
+fmp_api_calls_per_minute = 300
+with open(f"{local_financy}/.apikey") as f:
+    fmp_apikey = f.read().strip()
+fmp_log = f"{local_financy}/logs/fmp.log"
+
+reports_dir = f"{local_financy}/reports"
